831a7c14a53b01d7f54df24b2f42a64bc0c91821
natanimu <mimijaja@hotmail.com>
2020-06-08 10:04:49 -0700

    module6 assignment update

---
+++ javascript.js
@@ -0,0 +1,25 @@
+    document.getElementById("button1").addEventListener("click", function(){
+
+        document.getElementById("box").style.height = "25px";
+
+    });
+
+    document.getElementById("button2").addEventListener("click", function(){
+
+        document.getElementById("box").style.height = "250px";
+
+    });
+
+    document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "0px";
+
+    });
+ 
+
+    document.getElementById("button4").addEventListener("click", function(){
+
+        document.getElementById("box").style.height = "150px";
+
+    });
+
